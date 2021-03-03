534a796835f58b5bc897b8972082fc9c2735686a
yapfung <32047204+yapfung@users.noreply.github.com>
2021-03-03 16:19:58 +0800

    Add files via upload

---
+++ data/LDS_template_js.js
@@ -0,0 +1,23 @@
+var json_LDS = {
+"type": "FeatureCollection",
+"name": "LDS",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "lat": 1.5922, "lon": 103.8125, "class": "G", "datetime": "2020\/07\/23 07:07:50", "timedelta": 309.0 }, "geometry": { "type": "Point", "coordinates": [ 103.8125, 1.5922 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2622, "lon": 103.3523, "class": "C", "datetime": "2020\/07\/23 07:07:21", "timedelta": 309.0 }, "geometry": { "type": "Point", "coordinates": [ 103.3523, 1.2622 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2839, "lon": 103.484, "class": "C", "datetime": "2020\/07\/23 07:06:46", "timedelta": 310.0 }, "geometry": { "type": "Point", "coordinates": [ 103.484, 1.2839 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2951, "lon": 103.3539, "class": "C", "datetime": "2020\/07\/23 07:06:36", "timedelta": 310.0 }, "geometry": { "type": "Point", "coordinates": [ 103.3539, 1.2951 ] } },
+{ "type": "Feature", "properties": { "lat": 1.363, "lon": 103.6166, "class": "C", "datetime": "2020\/07\/23 07:06:24", "timedelta": 310.0 }, "geometry": { "type": "Point", "coordinates": [ 103.6166, 1.363 ] } },
+{ "type": "Feature", "properties": { "lat": 1.261, "lon": 103.3632, "class": "C", "datetime": "2020\/07\/23 07:05:22", "timedelta": 311.0 }, "geometry": { "type": "Point", "coordinates": [ 103.3632, 1.261 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2759, "lon": 103.4176, "class": "C", "datetime": "2020\/07\/23 07:14:55", "timedelta": 302.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4176, 1.2759 ] } },
+{ "type": "Feature", "properties": { "lat": 1.282, "lon": 103.5011, "class": "C", "datetime": "2020\/07\/23 07:14:55", "timedelta": 302.0 }, "geometry": { "type": "Point", "coordinates": [ 103.5011, 1.282 ] } },
+{ "type": "Feature", "properties": { "lat": 1.271, "lon": 103.4117, "class": "C", "datetime": "2020\/07\/23 07:14:37", "timedelta": 302.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4117, 1.271 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2522, "lon": 103.4063, "class": "C", "datetime": "2020\/07\/23 07:13:03", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4063, 1.2522 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2814, "lon": 103.4502, "class": "C", "datetime": "2020\/07\/23 07:13:03", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4502, 1.2814 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2755, "lon": 103.4313, "class": "C", "datetime": "2020\/07\/23 07:12:51", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4313, 1.2755 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2839, "lon": 103.4862, "class": "C", "datetime": "2020\/07\/23 07:12:51", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4862, 1.2839 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2781, "lon": 103.4706, "class": "C", "datetime": "2020\/07\/23 07:12:51", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4706, 1.2781 ] } },
+{ "type": "Feature", "properties": { "lat": 1.285, "lon": 103.4771, "class": "C", "datetime": "2020\/07\/23 07:12:32", "timedelta": 304.0 }, "geometry": { "type": "Point", "coordinates": [ 103.4771, 1.285 ] } },
+{ "type": "Feature", "properties": { "lat": 1.2567, "lon": 103.343, "class": "C", "datetime": "2020\/07\/23 07:10:14", "timedelta": 306.0 }, "geometry": { "type": "Point", "coordinates": [ 103.343, 1.2567 ] } }
+]
+}
